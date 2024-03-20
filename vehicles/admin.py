5c9fbb30f118a2248640cee8581d89fb307c1f3a
wsolano8 <wsolano@ltechec.com>
2024-03-20 11:29:09 -0500

    Registramos nuesta aplicacion en el admin.py

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vehicle
 
-# Register your models here.
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'model', 'year', 'price', 'image']  # Asegúrate de incluir 'image' aquí
+
+admin.site.register(Vehicle, VehicleAdmin)
