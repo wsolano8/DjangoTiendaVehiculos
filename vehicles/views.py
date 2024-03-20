1a62777d56e05c7f82d6eb0a6715ffb5766e0064
wsolano8 <wsolano@ltechec.com>
2024-03-20 11:37:13 -0500

    Creamos las vistas y el archivo forms.py

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Vehicle
+from .forms import VehicleForm
 
-# Create your views here.
+def vehicle_list(request):
+    vehicles = Vehicle.objects.all()
+    return render(request, 'vehicle_list.html', {'vehicle_list': vehicles})
+
+def vehicle_detail(request, vehicle_id):
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    return render(request, 'vehicle_detail.html', {'vehicle': vehicle})
+
+def add_vehicle(request):
+    if request.method == 'POST':
+        form = VehicleForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('vehicle_list')
+    else:
+        form = VehicleForm()
+    return render(request, 'add_vehicle.html', {'form': form})
+
+def edit_vehicle(request, vehicle_id):
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    if request.method == 'POST':
+        form = VehicleForm(request.POST, instance=vehicle)
+        if form.is_valid():
+            form.save()
+            return redirect('vehicle_list')
+    else:
+        form = VehicleForm(instance=vehicle)
+    return render(request, 'edit_vehicle.html', {'form': form})
+
+def delete_vehicle(request, vehicle_id):
+    vehicle = Vehicle.objects.get(id=vehicle_id)
+    vehicle.delete()
+    return redirect('vehicle_list')
